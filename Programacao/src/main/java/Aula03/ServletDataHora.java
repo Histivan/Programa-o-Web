3635b22bba2d773813dbd3a5b0afb88bcd34c841
Histivan <Histivan@DESKTOP-4LN31MC>
2021-08-19 17:29:46 -0300

    Aula Programação Web

---
+++ Programacao/src/main/java/Aula03/ServletDataHora.java
@@ -0,0 +1,52 @@
+package Aula03;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+    
+/**
+ * Servlet implementation class ServletDataHora
+ */
+@WebServlet("/ServletDataHora")
+public class ServletDataHora extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       private SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy - hh:mm:ss");
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDataHora() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init() throws ServletException {
+		log("ServletDataHora iniciado às" + format.format(new Date())); 
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
